cd2174271cf4008c198d29d15eeb62e7705d6613
nodaridev <nodari.pylypyshak@gmail.com>
2022-09-04 12:41:11 +0200

    remove grid

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri()?>/assets/images/favicon.png"/>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/styles/dist/style.min.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/styles/dist/grid.min.css">
 	<?php wp_head(); ?>
 </head>
 
